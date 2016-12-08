93d0da5855822048e227dbc99cbc67aa41d88c1b
Tavleen Singh <tavleensingh@Tavleens-MacBook-Air.local>
2016-12-07 23:06:31 -0800

    adding basic html structure and logic.js file

---
+++ assets/javascript/logic.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBk74DnDRxLE792TDt8waomTDPh6UOBRnk",
+    authDomain: "train-activity-week-7.firebaseapp.com",
+    databaseURL: "https://train-activity-week-7.firebaseio.com",
+    storageBucket: "https://train-activity-week-7.appspot.com",
+    messagingSenderId: "60404668303"
+  };
+  firebase.initializeApp(config);
